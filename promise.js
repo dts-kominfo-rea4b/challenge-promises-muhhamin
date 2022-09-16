9727c7f8a12d428d3d9b7a196ba56287858c6d5f
muhhamin <muhh.amin39@gmail.com>
2022-09-16 15:20:32 +0800

    Menambahkan fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (rasa) => {
+  try {
+    const IXX = await promiseTheaterIXX();
+    const VGC = await promiseTheaterVGC();
+    let i = 0;
+    IXX.forEach(hitung);
+    VGC.forEach(hitung);
+    function hitung(item, index) {
+      if (item.hasil==rasa){i++}
+    }
+    return i;
+
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
